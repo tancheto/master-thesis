132b639267100661fdc9b985aa0fcf9e69aa965f
Таня Христова <tanjash@office365student.uni-sofia.bg>
2023-09-26 14:58:56 +0300

    Add specificity (true negative rate) as a metric in evaluation of model performance

--- util.py
+++ util.py
@@ -5,16 +5,20 @@ from fairlearn.metrics import MetricFrame, demographic_parity_difference, equali
 from aif360.sklearn.metrics import equal_opportunity_difference
 
 def evaluate_model_performance(Y_target, Y_pred):
+
+    cm = confusion_matrix(Y_target, Y_pred)
+
     print('The accuracy score for the testing data:', accuracy_score(Y_target, Y_pred))
-    print('The balanced accuracy score for the testing data:', balanced_accuracy_score(Y_target, Y_pred))
     print('The precision score for the testing data:', precision_score(Y_target, Y_pred))
     print('The recall score for the testing data:', recall_score(Y_target, Y_pred))
     print('The F1 score for the testing data:', f1_score(Y_target, Y_pred))
     print('The F2 score for the testing data:', fbeta_score(Y_target, Y_pred, beta=2))
+    print('The specificity score for the testing data:', (cm[0][0] / (cm[0][0] + cm[0][1])))
+    print('The balanced accuracy score for the testing data:', balanced_accuracy_score(Y_target, Y_pred))
     print('The G mean score for the testing data:', geometric_mean_score(Y_target, Y_pred))
 
     #Ploting the confusion matrix
-    confusion_matrix(Y_target, Y_pred)
+    print(cm)
 
 def evaluate_model_fairness(Y_target, Y_pred, sensitive_features):
 
