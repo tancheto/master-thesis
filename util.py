d20e8daa114ba3f19cf5cae90c279c955c4e4695
Таня Христова <tanjash@office365student.uni-sofia.bg>
2024-01-21 21:58:44 +0200

    Add data undersampling

--- util.py
+++ util.py
@@ -1,24 +1,47 @@
+import os
+import pandas as pd
+
+from sklearn import metrics
+from sklearn.base import clone
+
 from sklearn.metrics import accuracy_score, balanced_accuracy_score, confusion_matrix, precision_score, recall_score, f1_score, fbeta_score
 from imblearn.metrics import geometric_mean_score
-
 from fairlearn.metrics import MetricFrame, demographic_parity_difference, equalized_odds_difference, selection_rate
-from aif360.sklearn.metrics import equal_opportunity_difference
+from aif360.sklearn.metrics import equal_opportunity_difference, disparate_impact_ratio
+
+from pathlib import Path
+import sys
+sys.path.append(os.path.abspath('..'))
 
 def evaluate_model_performance(Y_target, Y_pred):
 
     cm = confusion_matrix(Y_target, Y_pred)
+    
+    print()
+    print("===After training the model===")
 
-    print('The accuracy score for the testing data:', accuracy_score(Y_target, Y_pred))
-    print('The precision score for the testing data:', precision_score(Y_target, Y_pred))
-    print('The recall score for the testing data:', recall_score(Y_target, Y_pred))
-    print('The F1 score for the testing data:', f1_score(Y_target, Y_pred))
-    print('The F2 score for the testing data:', fbeta_score(Y_target, Y_pred, beta=2))
-    print('The specificity score for the testing data:', (cm[0][0] / (cm[0][0] + cm[0][1])))
-    print('The balanced accuracy score for the testing data:', balanced_accuracy_score(Y_target, Y_pred))
-    print('The G mean score for the testing data:', geometric_mean_score(Y_target, Y_pred))
+    accuracy = accuracy_score(Y_target, Y_pred)
+    precision = precision_score(Y_target, Y_pred)
+    recall = recall_score(Y_target, Y_pred)
+    f1 = f1_score(Y_target, Y_pred)
+    f2 = fbeta_score(Y_target, Y_pred, beta=2)
+    specificity = (cm[0][0] / (cm[0][0] + cm[0][1]))
+    bal_accuracy = balanced_accuracy_score(Y_target, Y_pred)
+    geom_mean = geometric_mean_score(Y_target, Y_pred)
+
+    print('The accuracy score for the testing data:', accuracy)
+    print('The precision score for the testing data:', precision)
+    print('The recall score for the testing data:', recall)
+    print('The F1 score for the testing data:', f1)
+    print('The F2 score for the testing data:', f2)
+    print('The specificity score for the testing data:', specificity)
+    print('The balanced accuracy score for the testing data:', bal_accuracy)
+    print('The G mean score for the testing data:', geom_mean)
 
     #Ploting the confusion matrix
     print(cm)
+
+    return [accuracy, precision, recall, f1, f2, specificity, bal_accuracy, geom_mean]
 
 def evaluate_model_fairness(Y_target, Y_pred, sensitive_features):
 
@@ -35,7 +58,7 @@ def evaluate_model_fairness(Y_target, Y_pred, sensitive_features):
     )
 
     # The disaggregated metrics
-    tree_mf.by_group
+    print(tree_mf.by_group)
 
     dem_par_diff = demographic_parity_difference(
         y_true=Y_target,
@@ -55,6 +78,134 @@ def evaluate_model_fairness(Y_target, Y_pred, sensitive_features):
             prot_attr=sensitive_features
     )
 
-    print('The Demographic parity difference score for the testing data:', dem_par_diff)
-    print('The Equalized odds difference score for the testing data:', eq_odd_diff)
-    print('The Equal opportunity difference score for the testing data:', eq_opp_diff)
+    disparate_impact_rat = disparate_impact_ratio(
+        y_true=Y_target,
+        y_pred=Y_pred,
+        prot_attr=sensitive_features,
+        priv_group='Caucasian',
+        pos_label=True
+    )
+
+    print('The Demographic parity difference score on the trained model:', dem_par_diff)
+    print('The Equalized odds difference score on the trained model:', eq_odd_diff)
+    print('The Equal opportunity difference score on the trained model:', eq_opp_diff)
+    print('The Disparate impact ratio on the trained model:', disparate_impact_rat)
+
+    return [dem_par_diff, eq_odd_diff, eq_opp_diff, disparate_impact_rat]
+    
+def evaluate_train_data_fairness(X_train, Y_train, A_train):
+        #fairness metrics to be used
+    metrics_dict = {
+        "selection_rate": selection_rate
+    }
+
+    tree_mf = MetricFrame(
+        metrics=metrics_dict,
+        y_true=Y_train,
+        y_pred=Y_train,
+        sensitive_features=A_train
+    )
+
+    # The disaggregated metrics
+    print()
+    print("===Before training the model===")
+    print(tree_mf.by_group)
+
+    dem_par_diff = demographic_parity_difference(
+        y_true=Y_train,
+        y_pred=Y_train,
+        sensitive_features=A_train
+    )
+
+    disparate_impact_rat = disparate_impact_ratio(
+        y_true=Y_train,
+        prot_attr=A_train,
+        priv_group='Caucasian',
+        pos_label=True
+    )
+
+    print('The Demographic parity difference score on the training data:', dem_par_diff)
+    print("The Disparate impact ratio on the training data:", disparate_impact_rat)
+
+    return [dem_par_diff, disparate_impact_rat]
+
+def train_model_on_datasets(model):
+
+    # load datasets
+    data_path = Path(os.getcwd()).parent.parent / "data" / "dataset_diabetes" / "clsf_data"
+
+    target_variable = "readmit_30_days"
+    sensitive_attribute = "race"
+
+    X_test = pd.read_csv(data_path / "X_test_split.csv")
+    X_A_test = pd.read_csv(data_path / "X_A_test_split.csv")
+    Y_test = pd.read_csv(data_path / "Y_test_split.csv")[target_variable]
+    A_test = pd.read_csv(data_path / "A_test_split.csv")[sensitive_attribute]
+
+    X_train = pd.read_csv(data_path / "X_train_split.csv")
+    X_A_train = pd.read_csv(data_path / "X_A_train_split.csv")
+    Y_train = pd.read_csv(data_path / "Y_train_split.csv")[target_variable]
+    A_train = pd.read_csv(data_path / "A_train_split.csv")[sensitive_attribute]
+
+    X_train_res_target_wos = pd.read_csv(data_path / "X_train_res_target_wos.csv")
+    Y_train_res_target_wos = pd.read_csv(data_path / "Y_train_res_target_wos.csv")[target_variable]
+    A_train_res_target_wos = pd.read_csv(data_path / "A_train_res_target_wos.csv").iloc[:, 0]
+
+    X_A_train_res_target_ws = pd.read_csv(data_path / "X_A_train_res_target_ws.csv")
+    Y_train_res_target_ws = pd.read_csv(data_path / "Y_train_res_target_ws.csv")[target_variable]
+    A_train_res_target_ws = pd.read_csv(data_path / "A_train_res_target_ws.csv").iloc[:, 0]
+
+    X_train_res_sensitive_wos = pd.read_csv(data_path / "X_train_res_sensitive_wos.csv")
+    Y_train_res_sensitive_wos = pd.read_csv(data_path / "Y_train_res_sensitive_wos.csv")[target_variable]
+    A_train_res_sensitive_wos = pd.read_csv(data_path / "A_train_res_sensitive_wos.csv").iloc[:, 0]
+
+    X_A_train_res_sensitive_ws = pd.read_csv(data_path / "X_A_train_res_sensitive_ws.csv")
+    Y_train_res_sensitive_ws = pd.read_csv(data_path / "Y_train_res_sensitive_ws.csv")[target_variable]
+    A_train_res_sensitive_ws = pd.read_csv(data_path / "A_train_res_sensitive_ws.csv").iloc[:, 0]
+
+    X_train_res_multiv_wos = pd.read_csv(data_path / "X_train_res_multiv_wos.csv")
+    Y_train_res_multiv_wos = pd.read_csv(data_path / "Y_train_res_multiv_wos.csv")[target_variable]
+    A_train_res_multiv_wos = pd.read_csv(data_path / "A_train_res_multiv_wos.csv").iloc[:, 0]
+
+    X_A_train_res_multiv_ws = pd.read_csv(data_path / "X_A_train_res_multiv_ws.csv")
+    Y_train_res_multiv_ws = pd.read_csv(data_path / "Y_train_res_multiv_ws.csv")[target_variable]
+    A_train_res_multiv_ws = pd.read_csv(data_path / "A_train_res_multiv_ws.csv").iloc[:, 0]
+
+    X_train_list = [X_train, X_A_train, X_train_res_target_wos, X_A_train_res_target_ws, X_train_res_sensitive_wos, \
+              X_A_train_res_sensitive_ws, X_train_res_multiv_wos, X_A_train_res_multiv_ws]
+
+    Y_train_list = [Y_train, Y_train, Y_train_res_target_wos, Y_train_res_target_ws, Y_train_res_sensitive_wos, \
+                    Y_train_res_sensitive_ws, Y_train_res_multiv_wos, Y_train_res_multiv_ws]
+
+    A_train_list = [A_train, A_train, A_train_res_target_wos, A_train_res_target_ws, A_train_res_sensitive_wos, \
+                    A_train_res_sensitive_ws, A_train_res_multiv_wos, A_train_res_multiv_ws]
+
+    X_test_list = [X_test, X_A_test, X_test, X_A_test, X_test, X_A_test, X_test, X_A_test]
+
+    dataset_ref = [ 'no resampling, w/o sensitive attribute', 'no resampling, with sensitive attribute', \
+                    'target-resample, w/o sensitive attribute', 'target-resample, with sensitive attribute', \
+                    'sensitive-resample, w/o sensitive attribute', 'sensitive-resample, with sensitive attribute', \
+                    'multivariate-resample, w/o sensitive attribute', 'multivariate-resample, with sensitive attribute']
+    
+    train_data_fairness_results = []
+    performance_results = []
+    fairness_results = []
+    
+    for (X_train_i, Y_train_i, A_train_i, X_test_i, ref_i) in zip(X_train_list, Y_train_list, A_train_list, X_test_list, dataset_ref):
+        print()
+        print(ref_i)
+        model_copy = clone(model)
+        model_copy.fit(X_train_i, Y_train_i)
+        # Predicting on the test data
+        model_pred = model_copy.predict(X_test_i)
+        train_data_fairness_results_i = evaluate_train_data_fairness(X_train_i, Y_train_i, A_train_i)
+        performance_results_i = evaluate_model_performance(Y_test, model_pred)
+        fairness_results_i = evaluate_model_fairness(Y_test, model_pred, A_test)
+
+        train_data_fairness_results.append(train_data_fairness_results_i)
+        performance_results.append(performance_results_i)
+        fairness_results.append(fairness_results_i)
+
+        print(metrics.RocCurveDisplay.from_estimator(model_copy, X_test_i, Y_test))
+
+    return train_data_fairness_results, performance_results, fairness_results
